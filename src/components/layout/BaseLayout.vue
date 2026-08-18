a8cccc3804f1ebd96824d8dc6289a26e7bf2256a
Fifaamonethep <amonethepthai@gmail.com>
2026-08-19 01:37:48 +0700

    Hide Navbar on login/register pages

--- src/components/layout/BaseLayout.vue
+++ src/components/layout/BaseLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-gray-50 dark:bg-gray-950 transition-colors duration-300 flex flex-col font-sans relative pb-16 md:pb-0">
-    <Navbar />
+    <Navbar v-if="authStore.user" />
     
     <main class="flex-grow max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8 relative">
       <router-view v-slot="{ Component }">
